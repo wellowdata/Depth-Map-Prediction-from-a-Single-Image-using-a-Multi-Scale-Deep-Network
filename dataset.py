789aa4ca418bdae67f1ce223bd2d633f27316f0d
Mark Wells <wellowdata@gmail.com>
2019-07-27 11:47:32 +0000

    from tens

--- dataset.py
+++ dataset.py
@@ -1,10 +1,15 @@
 import torch
 import h5py
+import io
+
+
 from PIL import Image
 import numpy as np
 from google.cloud import storage
+from torch.utils import data
 
-class NYUDataset(torch.utils.data.Dataset):
+
+class NYUDataset(data.Dataset):
     def __init__(self, data_dir, tfms):
         super(NYUDataset, self).__init__()
         self.data_dir = data_dir
@@ -40,10 +45,11 @@ class NYUDataset(torch.utils.data.Dataset):
         return self.len    
 
     
-class ADE20kDataset(torch.utils.data.Dataset):
+class ADE20kDataset(data.Dataset):
     def __init__(self, data_dir, tfms):
         super(ADE20kDataset, self).__init__()
-        self.data_dir = data_dir
+        #self.data_dir = data_dir
+        self.gen = ADE20k_iterator()
         self.tfms = tfms
         
         #self.ds_v_1 = h5py.File(self.data_dir+'nyu_depth_data_labeled.mat')
@@ -53,33 +59,65 @@ class ADE20kDataset(torch.utils.data.Dataset):
 
            
     def __getitem__(self, index):
-        if(index<len(self.ds_v_1["images"])):
-            ds = self.ds_v_1    
-            i = index
-        else:    
-            ds = self.ds_v_2
-            i = index - len(self.ds_v_1["images"])
-
-        img = np.transpose(ds["images"][i], axes=[2,1,0])
+#         if(index<len(self.ds_v_1["images"])):
+#             ds = self.ds_v_1    
+#             i = index
+#         else:    
+#             ds = self.ds_v_2
+#             i = index - len(self.ds_v_1["images"])
+        img, filepath = next(self.gen)
+        img = np.transpose(img, axes=[2,1,0])
         img = img.astype(np.uint8)
 
-        depth = np.transpose(ds["depths"][i], axes=[1,0])
-        depth = (depth/depth.max())*255
-        depth = depth.astype(np.uint8)
+        depth = None #np.transpose(ds["depths"][i], axes=[1,0])
+        #depth = (depth/depth.max())*255
+        #depth = depth.astype(np.uint8)
 
         if self.tfms:
-            tfmd_sample = self.tfms({"image":img, "depth":depth})
-            img, depth = tfmd_sample["image"], tfmd_sample["depth"]
+            tfmd_sample = self.tfms({"image":img, "depth":None})  #this is a major fudge to fudge it until it worked
+            img, depth = tfmd_sample["image"], tfmd_sample["depth"]  
         return (img, depth)
+    
+    def __len__(self):
+        count=0
+        client = storage.Client()
+        bucket = client.get_bucket('ucl-interior-desing')
+        prefix='ADE20K'
+        #/ADE20K_2016_07_26/images/validation/b/ballroom/'
+        blobs = bucket.list_blobs(prefix=prefix)
+        for blob in blobs:
+            if blob.name.endswith('.jpg'):
+                count +=1
+        return count    
+
     
 def ADE20k_iterator():
     client = storage.Client()
     bucket = client.get_bucket('ucl-interior-desing')
     prefix='ADE20K'
-    #/ADE20K_2016_07_26/images/validation/b/ballroom/'
     blobs = bucket.list_blobs(prefix=prefix)
     for blob in blobs:
         if blob.name.endswith('.jpg'):
-            blob.download_to_filename('temp')
-            img = Image.open('temp')
-            yield img
+            contenido = blob.download_as_string()
+            fp = io.BytesIO(contenido)
+            img = Image.open(fp)
+            yield img, blob.name
+            
+            
+if __name__ == "__main__":
+    from custom_transforms import *
+    bs = 8
+    sz = (320,240)
+    mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    mean, std = torch.tensor(mean), torch.tensor(std)
+
+    test_tfms = transforms.Compose([
+        transforms.Resize((240,320)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+        ])
+    #ds = NYUDataset('data/', tfms)
+    ds = ADE20kDataset('', test_tfms)
+    dl = torch.utils.data.DataLoader(ds, bs, shuffle=False)
+    img, depth = iter(dl).next()
+    print(img)
